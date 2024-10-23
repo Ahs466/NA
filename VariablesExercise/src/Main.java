d528839b88694e0dabfef4b3918267cd56766846
ANIKA <anikahussain37@gmail.com>
2024-10-23 15:52:10 +0100

    CREATE task two completed

--- VariablesExercise/src/Main.java
+++ VariablesExercise/src/Main.java
@@ -7,5 +7,18 @@ public class Main {
         lastName = "Husslayes";
         String name = firstName + " " + lastName;
         System.out.printf("%s\n", name);
+
+        int num1, num2;
+        num1 = 5;
+        num2 = 4;
+        System.out.printf("%s\n", num1 + num2);
+
+        double result = num1 / num2;
+        System.out.printf("%s\n", result);
+
+        int num3 = 8;
+        System.out.printf("%s %s %s\n", num3++, num3, ++num3);
+
+
     }
 }
